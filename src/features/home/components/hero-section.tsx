83245f9fc7ecbfa339b3d9f0f2bbfb7d8f5f6089
Kei-K23 <keiksl2301@gmail.com>
2024-12-08 12:29:16 +0630

    chore: refactor navbar header component to be align with dark and light modes

---
+++ src/features/home/components/hero-section.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <section className="relative">
+      <div className="flex items-center justify-between gap-x-5">
+        <div>
+          <h2 className="text-[#603809]">Discover the best coffee</h2>
+          <p>
+            Bean Scene is a coffee shop that provides you with quality coffee
+            that helps boost your productivity and helps build your mood. Having
+            a cup of coffee is good, but having a cup of real coffee is greater.
+            There is no doubt that you will enjoy this coffee more than others
+            you have ever tasted.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
